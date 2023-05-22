afcc95b2d83977e7f9cec941a64cc5199e1a34b8
Keks <keks@htmlacademy.ru>
2023-05-22 05:59:59 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/script.js
@@ -0,0 +1,1 @@
+const mainHeader=document.querySelector(".main-header"),navButton=document.querySelector(".nav-button");mainHeader.classList.remove("no-js"),navButton.addEventListener("click",(()=>mainHeader.classList.toggle("is-nav-open")));
